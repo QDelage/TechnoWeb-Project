64b42c0c0d2ac3faf43034ced9433e75e9e11436
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2020-09-27 17:20:03 +0200

    Début formulaire

--- include/pages/accueil.inc.php
+++ include/pages/accueil.inc.php
@@ -3,7 +3,7 @@
 
 <div class="row">
     <div class="col"></div>
-    <div class="col border p-3">
+    <div class="col-6 border p-3 ">
 
         <!-- Liste des sports de la DB -->
         <label>Sports :</label>
@@ -33,6 +33,25 @@
             </tr>
         </tbody>
         </table>
+        <form>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                </div>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
+
+    
     <div class="col"></div>
 </div>

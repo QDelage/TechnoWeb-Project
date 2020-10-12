24d539d757f2af9f2579c017ec3790b446631456
PaulPont <60519001+PaulPont@users.noreply.github.com>
2020-10-12 17:18:29 +0200

    Mise en ligne recherche

--- include/pages/recherche.php
+++ include/pages/recherche.php
@@ -1,1 +1,34 @@
 <h1 class="text-center mt-4">Recherche</h1>
+
+<div class="container-fluid">
+<div class="w-50">
+    <div class="col"></div>
+    <div class="col-8 border p-3 ">
+        <form class="form-group">
+            <div class="form-group">
+                <select id="sport" class="custom-select">
+                    <?php
+                    //faire liaison base de donnée select
+                    ?>
+
+                </select>
+            </div>
+            <div class="form-group">
+                <select id="niveau" class="custom-select">
+                    <?php
+                    //faire liaison base de donnée select
+                    ?>
+                </select>
+            </div>
+            <div class="form-group">
+
+                <select id="departement" class="custom-select">
+                    <?php
+                    //faire liaison base de donnée select
+                    ?>
+                </select>
+            </div>
+    </div>
+</div>
+</div>
+</form>

948427c8126e4f78b1a826f08d47f8ec4d54e856
PaulPont <60519001+PaulPont@users.noreply.github.com>
2020-10-12 18:44:01 +0200

    fix recherche

--- include/pages/recherche.php
+++ include/pages/recherche.php
@@ -1,34 +1,38 @@
 <h1 class="text-center mt-4">Recherche</h1>
 
 <div class="container-fluid">
-<div class="w-50">
-    <div class="col"></div>
-    <div class="col-8 border p-3 ">
-        <form class="form-group">
-            <div class="form-group">
-                <select id="sport" class="custom-select">
-                    <?php
-                    //faire liaison base de donnée select
-                    ?>
+        <div class="row">
+        <div class="col"></div>
 
-                </select>
-            </div>
-            <div class="form-group">
-                <select id="niveau" class="custom-select">
-                    <?php
-                    //faire liaison base de donnée select
-                    ?>
-                </select>
-            </div>
-            <div class="form-group">
+        <div class="col-9 border p-3 ">
 
-                <select id="departement" class="custom-select">
-                    <?php
-                    //faire liaison base de donnée select
-                    ?>
-                </select>
-            </div>
+            <form class="form-group">
+                <div class="form-group">
+                    <select id="sport" class="custom-select">
+                        <?php
+                        //faire liaison base de donnée select
+                        ?>
+
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select id="niveau" class="custom-select">
+                        <?php
+                        //faire liaison base de donnée select
+                        ?>
+                    </select>
+                </div>
+                <div class="form-group">
+
+                    <select id="departement" class="custom-select">
+                        <?php
+                        //faire liaison base de donnée select
+                        ?>
+                    </select>
+                </div>
+        </div>
+        <div class="col"></div>
     </div>
-</div>
+    </div>
 </div>
 </form>

709d03776da47db99af172d4c2a28dba992347ee
Quentin Delage <quentin.delage1@etu.unilim.fr>
2020-10-13 14:02:39 +0200

    Configuration générale SQL

--- include/config.inc.php
+++ include/config.inc.php
@@ -3,9 +3,9 @@
 // À modifier en fonction de la configuration
 
 define('DBHOST', "localhost");
-define('DBNAME', "PT");
-define('DBUSER', "userName");
-define('DBPASSWD', "mdp");
+define('DBNAME', "SportyMeet");
+define('DBUSER', "sportymeet");
+define('DBPASSWD', "sportymeet");
 define('ENV','dev');
 // pour un environememnt de production remplacer 'dev' (développement) par 'prod' (production)
 // les messages d'erreur du SGBD s'affichent dans l'environememnt dev mais pas en prod

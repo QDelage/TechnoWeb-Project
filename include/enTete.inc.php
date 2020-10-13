fb7096461cccc46a998a7f0833e5204141730184
Quentin Delage <quentin.delage1@etu.unilim.fr>
2020-10-13 15:04:38 +0200

    Titre Site avec font et plus gros

--- include/enTete.inc.php
+++ include/enTete.inc.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light">
   <a id="nomSite" class="navbar-brand" href="index.php?page=0">
     <img id="logo" src="img/logo.png" class="d-inline-block align-top" alt="logo">
-    Sporty'Meet
+    <h1 id="TitreSite">Sporty'Meet</h1>
   </a>
   <a class="btn btn-sm btn-dark mr-5 text-light btn-outline-secondary"
     href="index.php?page=1">Inscription</a>

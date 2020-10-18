712133f0462094529af9a4dea4b858265d6c2bba
PaulPont <60519001+PaulPont@users.noreply.github.com>
2020-10-18 17:34:10 +0200

    Creation sportmanager

---
+++ classes/SportManager.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/***
+ * Classe pour interfacer le serveur PHP au serveur MySQL
+ */
+class SportManager {
+    private $db;
+
+    public function __construct($db){
+        $this->db = $db;
+    }
+
+    public function getAllDepartments($id){
+        $controles = Array();
+
+        $sql = 'SELECT ID_SPORT, NOM 
+                    FROM SPORT';
+
+        $requete = $this->db->prepare($sql);
+        $requete->execute();
+
+        while ($sport = $requete->fetch(PDO::FETCH_OBJ)) {
+            $sports[] = new Sport($sport);
+        }
+        $requete->closeCursor();
+
+        return $sports;
+    }
+}
+

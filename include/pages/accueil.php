4251741534a3c6e089faabadba44b8a38458bbcc
Quentin Delage <quentin.delage1@etu.unilim.fr>
2020-10-29 17:24:05 +0100

    input required pour éviter les erreurs

--- include/pages/accueil.php
+++ include/pages/accueil.php
@@ -42,12 +42,12 @@
                 <div class="input-group-prepend">
                     <span class="input-group-text" id="basic-addon1">@</span>
                 </div>
-                <input name="mail" type="email" class="form-control" placeholder="Entrez votre adresse mail" aria-label="Mail" aria-describedby="basic-addon1">
+                <input required name="mail" type="email" class="form-control" placeholder="Entrez votre adresse mail" aria-label="Mail" aria-describedby="basic-addon1">
             </div>
 
             <div class="form-group">
                 <label for="exampleInputPassword1">Mot de passe</label>
-                <input name="motdepasse" type="password" class="form-control" id="exampleInputPassword1" placeholder="Entrez votre mot de passe">
+                <input required name="motdepasse" type="password" class="form-control" id="exampleInputPassword1" placeholder="Entrez votre mot de passe">
             </div>
 
             <button type="submit" class="btn btn-primary">Connexion</button>

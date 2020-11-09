493f4b2fa943e47e058d495f88d0c7bb0da259e9
Quentin DELAGE <quentin.delage1@etu.unilim.fr>
2020-11-09 13:03:22 +0100

    FrontEnd modification profil

--- include/pages/profil.php
+++ include/pages/profil.php
@@ -1,6 +1,11 @@
 <h1 class="text-center mt-4">Profil</h1>
 <div class="container-fluid">
 
+<?php
+// Pour éviter les problèmes, il a été décidé de ne modifier qu'un seul champ à la fois
+// La modification d'un profil étant rare, celà ne devrait pas trop impacter le client
+?>
+
 <div class="row">
     <div class="col"></div>
     <div class="col-5 p-3 text-center">
@@ -9,25 +14,85 @@
 
         <label>Nom : <?php 
         print $_SESSION['pers']->getNom(); ?>
-        </label><br /><br />
+        </label>
+        <!-- Bouton pour afficher le formulaire de modification de CET élément -->
+        <button class="btn btn-sm btn-outline-secondary" onclick="modifierProfilAfficherChamp('nom');">Modifer</button>
+        <br /><br />
+        <form id="profilFormNom" class="hide mb-4" method="post" action="">
+            <!-- Formulaire pour modifier le nom -->
+            <label>Nouveau nom : </label>
+            <input type="text">
+
+            <input class="btn btn-primary btn-sm" type="submit" value="OK">
+            <button onclick="modifierProfilCacherChamp('nom');" class="btn btn-danger btn-sm" type="button">X</button>
+        </form>
 
         <label>Prénom : <?php 
         print $_SESSION['pers']->getPrenom(); ?>
-        </label><br /><br />
+        </label>
+        <button class="btn btn-sm btn-outline-secondary" onclick="modifierProfilAfficherChamp('prenom');">Modifer</button>
+        <br /><br />
+        <form id="profilFormPrenom" class="hide mb-4" method="post" action="">
+            <!-- Formulaire pour modifier le prénom -->
+            <label>Nouveau prénom : </label>
+            <input type="text">
+
+            <input class="btn btn-primary btn-sm" type="submit" value="OK">
+            <button onclick="modifierProfilCacherChamp('prenom');" class="btn btn-danger btn-sm" type="button">X</button>
+        </form>
 
         <label>Département : <?php
         $dptMgr = new DepartementManager($pdo);
         $dpt = $dptMgr->getDepartement($_SESSION['pers']->getIdDepartement());
+        $dpts = $dptMgr->getAllDepartments();
         print $dpt->getNom(); ?>
-        </label><br /><br />
+        </label>
+        <button class="btn btn-sm btn-outline-secondary" onclick="modifierProfilAfficherChamp('departement');">Modifer</button>
+        <br /><br />
+        <form id="profilFormDepartement" class="hide mb-4" method="post" action="">
+            <!-- Formulaire pour modifier le département -->
+            <label>Nouveau département : </label>
+            <select required id="departement" name="departement" class="custom-select w-50">
+                <option selected disabled>Choisissez votre département</option>
+                <?php
+                foreach ($dpts as $key => $value) { ?>
+
+                <option value="<?php echo $value->getIdDepartement(); ?>"><?php echo $value->getNom(); ?></option>
+                
+                <?php } ?>
+            </select>
+
+            <input class="btn btn-primary btn-sm" type="submit" value="OK">
+            <button onclick="modifierProfilCacherChamp('departement');" class="btn btn-danger btn-sm" type="button">X</button>
+        </form>
 
         <label>Mail : <?php 
         print $_SESSION['pers']->getMail(); ?>
-        </label><br /><br />
+        </label>
+        <button class="btn btn-sm btn-outline-secondary" onclick="modifierProfilAfficherChamp('mail');">Modifer</button>
+        <br /><br />
+        <form id="profilFormMail" class="hide mb-4" method="post" action="">
+            <!-- Formulaire pour modifier le prénom -->
+            <label>Nouvelle adresse mail : </label>
+            <input type="mail">
+
+            <input class="btn btn-primary btn-sm" type="submit" value="OK">
+            <button onclick="modifierProfilCacherChamp('mail');" class="btn btn-danger btn-sm" type="button">X</button>
+        </form>
 
         <label>Mot de passe :
         *******
-        </label><br /><br />
+        </label>
+        <button class="btn btn-sm btn-outline-secondary" onclick="modifierProfilAfficherChamp('mdp');">Modifer</button>
+        <br /><br />
+        <form id="profilFormMDP" class="hide mb-4" method="post" action="">
+            <!-- Formulaire pour modifier le prénom -->
+            <label>Nouvelle adresse mail : </label>
+            <input type="password">
+
+            <input class="btn btn-primary btn-sm" type="submit" value="OK">
+            <button onclick="modifierProfilCacherChamp('mdp');" class="btn btn-danger btn-sm" type="button">X</button>
+        </form>
 
     </div>
     <div class="col"></div>

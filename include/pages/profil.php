3b982b5390f67ee32d38e890b22122fe71397854
PaulPont <60519001+PaulPont@users.noreply.github.com>
2020-12-05 17:57:42 +0100

    erreur }

--- include/pages/profil.php
+++ include/pages/profil.php
@@ -22,90 +22,90 @@
                 print '<p>Redirection vers l\'accueil en cours...</p>';
                 header('refresh:2;url=index.php');
             } else {
-            $prsnMngr = new PersonneManager($pdo);
-            $personne = $prsnMngr->getPersonne($_POST['id']); ?>
+                $prsnMngr = new PersonneManager($pdo);
+                $personne = $prsnMngr->getPersonne($_POST['id']); ?>
 
 
-            <img id="ImgProfil" class="img-circle mb-4 d-none d-sm-inline"
-                 src="img/profils/<?php print $personne->getPhoto(); ?>"><br/>
-            <img id="ImgProfilSmall" class="img-circle mb-4 d-sm-none"
-                 src="img/profils/<?php print $personne->getPhoto(); ?>"><br/>
+                <img id="ImgProfil" class="img-circle mb-4 d-none d-sm-inline"
+                     src="img/profils/<?php print $personne->getPhoto(); ?>"><br/>
+                <img id="ImgProfilSmall" class="img-circle mb-4 d-sm-none"
+                     src="img/profils/<?php print $personne->getPhoto(); ?>"><br/>
 
 
-            <label>Nom : <?php
-                print $personne->getNom(); ?>
-            </label>
-            <br/><br/>
+                <label>Nom : <?php
+                    print $personne->getNom(); ?>
+                </label>
+                <br/><br/>
 
 
-            <label>Prénom : <?php
-                print $personne->getPrenom(); ?>
-            </label>
-            <br/><br/>
+                <label>Prénom : <?php
+                    print $personne->getPrenom(); ?>
+                </label>
+                <br/><br/>
 
 
-            <label>Département : <?php
-                $dptMgr = new DepartementManager($pdo);
-                $dpt = $dptMgr->getDepartement($personne->getIdDepartement());
-                $dpts = $dptMgr->getAllDepartments();
-                print $dpt->getNom(); ?>
-            </label>
-            <br/><br/>
-            <label>Description : <?php
-                print $personne->getDescription(); ?>
-            </label>
-            <br/><br/>
-            <br/><br/>
+                <label>Département : <?php
+                    $dptMgr = new DepartementManager($pdo);
+                    $dpt = $dptMgr->getDepartement($personne->getIdDepartement());
+                    $dpts = $dptMgr->getAllDepartments();
+                    print $dpt->getNom(); ?>
+                </label>
+                <br/><br/>
+                <label>Description : <?php
+                    print $personne->getDescription(); ?>
+                </label>
+                <br/><br/>
+                <br/><br/>
 
-            <?php
+                <?php
 
-            if ($match) {
-                if ($match->getIdPersonne1() == $_SESSION['pers']->getidPersonne() && !$match->isReciproque()) {
-                    print '<p>Vous avez déjà liké cette personne</p>';
+                if ($match) {
+                    if ($match->getIdPersonne1() == $_SESSION['pers']->getidPersonne() && !$match->isReciproque()) {
+                        print '<p>Vous avez déjà liké cette personne</p>';
+                    } else {
+                        print '<p>Vous plaisez à cette personne !</p>';
+                    }
+
+                    if ($match->isReciproque()) {
+                        print '<p>Vous vous plaisez mutuellement !</p>';
+                    } else {
+                        $pmMgr->validerMatch($_SESSION['pers']->getidPersonne(), $_POST['id']);
+                        print '<p>Vous vous plaisez mutuellement !</p>';
+                    }
                 } else {
-                    print '<p>Vous plaisez à cette personne !</p>';
+                    print '<p>En attente...</p>';
+
+                    $pmMgr->createMatchEntre($_SESSION['pers']->getidPersonne(), $_POST['id']);
                 }
 
-                if ($match->isReciproque()) {
-                    print '<p>Vous vous plaisez mutuellement !</p>';
-                } else {
-                    $pmMgr->validerMatch($_SESSION['pers']->getidPersonne(), $_POST['id']);
-                    print '<p>Vous vous plaisez mutuellement !</p>';
+                ?>
+
+                <form method="post" action="index.php?page=4" <?php
+                // Si on a déjà liké cette personne (en premier, ou en second)
+                if ($match != false) {
+                    if (isset($match) && ($match->getIdPersonne1() == $_SESSION['pers']->getidPersonne() || $match->isReciproque())) {
+                        print ' hidden ';
+                    }
                 }
-            } else {
-                print '<p>En attente...</p>';
+                ?>>
+                    <!-- Pour garder l'ID sur la page réactualisée -->
+                    <label>Vous aimez cette personne ?</label><br/>
+                    <input hidden name="id" value="<?php print $_POST['id']; ?>">
 
-                $pmMgr->createMatchEntre($_SESSION['pers']->getidPersonne(), $_POST['id']);
-            }
+                    <button type="submit" name="like" value="<?php print $_POST['id']; ?>" class="btn btn-success">
+                        Liker
+                    </button>
 
-            ?>
+                </form>
 
-            <form method="post" action="index.php?page=4" <?php
-            // Si on a déjà liké cette personne (en premier, ou en second)
-            if ($match != false) {
-                if (isset($match) && ($match->getIdPersonne1() == $_SESSION['pers']->getidPersonne() || $match->isReciproque())) {
-                    print ' hidden ';
-                }
-            }
-            ?>>
-                <!-- Pour garder l'ID sur la page réactualisée -->
-                <label>Vous aimez cette personne ?</label><br/>
-                <input hidden name="id" value="<?php print $_POST['id']; ?>">
-
-                <button type="submit" name="like" value="<?php print $_POST['id']; ?>" class="btn btn-success">
-                    Liker
-                </button>
-
-            </form>
-
-            <?php
-            if ($match != false) {
-                if (isset($match) && ($match->getIdPersonne1()) == $_SESSION['pers']->getidPersonne()) {
-                    print '<br/><br/></br.><p>Vous avez déjà liké cette personne. Attendez sa réponse !</p>';
+                <?php
+                if ($match != false) {
+                    if (isset($match) && ($match->getIdPersonne1()) == $_SESSION['pers']->getidPersonne()) {
+                        print '<br/><br/></br.><p>Vous avez déjà liké cette personne. Attendez sa réponse !</p>';
+                    }
                 } ?>
-                }
-            <?php }
-            }?>
+
+            <?php } ?>
 
 
         </div>
